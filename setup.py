019142d71f8d431bdcc2a633cdbc2064de54cc08
Palani <palanioffcl@gmail.com>
2022-12-13 17:31:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,17 @@
 from setuptools import setup
 
 setup(
-
     name="radox",
-
     keywords=["wsgi", "framework", ],
-
     version="0.1",
-
     maintainer="Palani",
-
     description="Another wsgi based python web framework",
-
     packages=["radox"],
-
     classifiers=[
-
         "Development Status :: 3 - Alpha",
-
         "Environment :: Web",
-
         "Programming Language :: Python :: 3",
-
         "Topic :: Internet :: WWW/HTTP",
-
         "Topic :: Software Development :: Libraries :: Web Framework",
-
-    ],
-
+    ]
 )
