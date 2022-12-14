66c9a77b1802d62e8e706684d428e96aad2aa923
Palani <palanioffcl@gmail.com>
2022-12-14 16:12:11 +0530

    Update response.py

--- src/response.py
+++ src/response.py
@@ -1,4 +1,4 @@
-from . import Request
+from src.request import Request
 
 file_types = {
     'html': 'text/html',
@@ -6,25 +6,27 @@ file_types = {
 }
 
 class Response:
-    __slots__ = 'headers', 'status_code', 'start_response', 'content_type', 'response_content'
+    __slots__ = 'headers', 'status_code', 'make_response', 'content_type', 'response_content'
 
     def __init__(self,request: Request, status_code: str, content_type: str):
         self.headers = []
         self.status_code = status_code
-        self.start_response = request.start_response
+        self.make_response = request.make_response
         self.content_type = content_type
         self.response_content = []
         
     def response(self):
-        self.start_response(self.status_code, [('Content-Type', self.content_type)])
+        self.make_response(self.status_code, [('Content-Type', self.content_type)])
         return self.response_content         
 
-class Http(Response):
+
+class HttpResponse(Response):
     def __init__(self, request: Request, content, status_code='200 OK', content_type='text/html'):
         super().__init__( request, status_code, content_type)
         if type(content) == str:
             content = content.encode()
         self.response_content.append(content)
+
         
 class Render(HttpResponse):
     def __init__(self, request: Request, filename: str, context: dict = {}):
@@ -36,6 +38,13 @@ class Render(HttpResponse):
             raise Exception(f'File Could not be found! {filename}')
         super().__init__(request, text, '200 OK')
 
+
 class Error(Response):
     def __init__(self, request: Request, error_code: str):
+        super().__init__(request, '404 Not Found', 'text/html')
+        self.response_content.append("404 Not Found".encode())
+
+
+class Http404(Error):
+    def __init__(self, request):
         super().__init__(request, '404 Not Found')
