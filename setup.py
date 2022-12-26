a203b0e6dd6a73c3e71dab38905cba6314496cc3
Palani <palanioffcl@gmail.com>
2022-12-26 13:36:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
   name = 'radox',
-  packages = ['wsgi','framework'],
-  version = '0.0.1',
-  description = 'A WSGI based Python web framework',
+  packages = find_packages(),
+  version = '0.0.4',
+  description = 'A WSGI based Python web framework inspired from flask',
   author = 'Palani',
   author_email = 'palanioffcl@gmail.com',
-  url = 'https://github.com/codeverything/radox',
+  install_requires=['wsgiref'],
   keywords = ['web', 'python', 'framework', 'libraries', 'webapps'],
   classifiers = [
         "Development Status :: 3 - Alpha",
-        "Environment :: Web",
-        "Programming Language :: Python :: 3",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Software Development :: Libraries :: Web Framework"
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Internet :: WWW/HTTP"
     ]
 )
