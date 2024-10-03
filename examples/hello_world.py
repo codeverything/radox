594cbba4b4cdf4c36a494c34ba8263c281d298b8
Onyekachi Obasi <kobasi896@gmail.com>
2024-10-04 00:37:39 +0100

    update

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,18 +1,25 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.main import *
-from utils.srv_info import *
 from wsgiref.simple_server import make_server
 
-app = radox()
+app = Radox()
 
 @app.route("/")
 def index():
     return "Hello World"
 
 @app.route("/home")
-def index(request):
+def home():
     return "Home Page"
 
-if __name__=='__main__':
+def info():
+    print("Server running at http://127.0.0.1:3301")
+
+if __name__ == '__main__':
     server = make_server('127.0.0.1', 3301, app)
     info()
     server.serve_forever()
--- src/main.py
+++ src/main.py
@@ -1,24 +1,21 @@
 from wsgiref.simple_server import make_server
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from socketserver import *
+from socketserver import TCPServer
 
-
-class radox:
+class Radox:
     def __init__(self):
         self.routes = {}
 
     def route(self, path):
         def decorator(handler):
             self.routes[path] = handler
-            def wrapper():
-                return handler()
-            return wrapper
+            return handler
         return decorator
 
     def serve(self, port):
         handler = self.make_handler()
-        with socketserver.TCPServer(("", port), handler) as httpd:
-            print("Serving on port", port)
+        with TCPServer(("", port), handler) as httpd:
+            print(f"Serving on port {port}")
             httpd.serve_forever()
 
     def __call__(self, environ, start_response):
@@ -28,28 +25,33 @@ class radox:
             start_response('404 Not Found', [('Content-Type', 'text/plain')])
             return [b'404 Not Found']
         else:
-             start_response('200 OK', [('Content-Type', 'text/plain')])
-             response = self.routes[path]()
-             return [response.encode()]
+            start_response('200 OK', [('Content-Type', 'text/plain')])
+            response = self.routes[path]()
+            return [response.encode()]
 
-    def render_template(template_name):
+    def render_template(self, template_name):
         with open(template_name, "r") as f:
-           return f.read()
+            return f.read()
 
     def make_handler(self):
-        class RequestHandler(http.server.BaseHTTPRequestHandler):
+        # Nested RequestHandler class inheriting from BaseHTTPRequestHandler
+        class RequestHandler(BaseHTTPRequestHandler):
             def do_GET(self):
                 if self.path in self.server.web_framework.routes:
                     handler = self.server.web_framework.routes[self.path]
-                    handler(self)
+                    response = handler()
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(response.encode())
                 else:
                     self.send_response(404)
                     self.end_headers()
                     self.wfile.write(b"404 Not Found")
 
-        class TCPServer(socketserver.TCPServer):
+        # this class will integrate the web framework
+        class CustomTCPServer(TCPServer):
             def __init__(self, *args, **kwargs):
                 self.web_framework = kwargs.pop("web_framework")
                 super().__init__(*args, **kwargs)
 
-        return TCPServer(web_framework=self)
+        return CustomTCPServer(web_framework=self)
